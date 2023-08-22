aae179e6a4acc3e3f1f94723fb3fb795018bb176
Harika <harikamudhiraj121@gmail.com>
2023-08-22 16:07:32 +0530

    PYTHON

---
+++ PYTHON/hello.py
@@ -0,0 +1,15 @@
+#print("hello world\n") # u can keep doublequotes or single quotes
+
+
+#print("('hello')")
+
+#print("hello\nhello\nhello\n")
+
+#print("hello" + "jenny") #concatinate two strings it will print output without space
+
+
+
+
+#print("hello " + "jenny") #concatinate two strings it will print output with space
+#print("hello" + " jenny") #concatinate two strings it will print output with space
+print("hello"+" "+"jenny") #concatinate two strings it will print output with space
