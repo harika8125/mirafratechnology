8ab380d2e272f0ba81d5d2d0dcf644caf7734143
Harika <harikamudhiraj121@gmail.com>
2023-10-27 15:48:18 +0530

    Python_exer

---
+++ PYTHON/python_exercise/1_even_odd.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number\n"))
+if a%2==0:
+    print("even\n")
+else:
+    print("odd\n")
+
+
+#using functions
+
+def even_odd(n):
+ if n%2==0:
+    print("even")
+ else:
+    print("odd")
+n=int(input("enter a number"))
+even_odd(n)
+~
